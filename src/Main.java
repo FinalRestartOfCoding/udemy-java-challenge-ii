7a19c3d938a7d66b2d1ff6ab2c24f09eae7ea155
FinalRestartOfCoding <finalrestartofcoding@gmail.com>
2022-10-28 16:04:32 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        // Initializing a second variable via value.
+        int mySecondNumber = 12;
+
+        // Initializing a third variable via variable.
+        int myThirdNumber = myFirstNumber * 2;
+
+        // Using variables in an expression.
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        // Prints the value stored in variable myTotal.
+        System.out.println(myTotal);
+    }
+}
